abc11c67ec3dc83f27de7be6416bfd2017f70938
Nicolas <nic.jundiai@gmail.com>
2023-03-31 11:20:37 -0300

    criação do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MercadoSantos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
